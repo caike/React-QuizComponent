6ec376db405eb8737a5534c93b18ffca67b4aaa7
Jon Friskics <jonfriskics@gmail.com>
2018-05-01 12:20:04 -0400

    module 5 tests

---
+++ src/__tests__/part6/quiz-handle-click-increments-position-state.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../../App';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
+import sinon from 'sinon';
+
+let quizComponentExists = false;
+let Quiz;
+try {
+  Quiz = require('../../Quiz.js').default;
+  quizComponentExists = true;
+} catch (e) {
+  quizComponentExists = false;
+}
+
+let fs = require('fs');
+let quiz_data = require('../../quiz_data.json')
+let babylon = require('babylon')
+
+describe('Quiz Component', () => {
+  it('handleClick() method increments the quiz_position by 1 @quiz-handle-click-increments-position-state', () => {
+    assert(quizComponentExists, "The Quiz component hasn't been created yet.")
+
+    let quiz;
+
+    try {
+      quiz = shallow(<Quiz />)
+    } catch (e) {
+      assert(false, "We weren't able to mount the Quiz component")
+    }
+
+    if (quiz.state().quiz_position) {
+      assert(quiz.state().quiz_position == quiz_data.quiz_position, "The Quiz component's state does not have a key named `quiz_position` with the correct value - are you sure you're still setting the component's state to `quiz_data`?")
+      quiz.instance().handleClick()
+      assert(quiz.state().quiz_position == quiz_data.quiz_position+1, "The Quiz component state's `quiz_position` value is not being incremented by 1 when the `handleClick()` method is called.")
+    }
+  })
+})
---
+++ src/__tests__/part6/quiz-has-onclick-handler.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../../App';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
+import sinon from 'sinon';
+
+let quizComponentExists = false;
+let Quiz;
+try {
+  Quiz = require('../../Quiz.js').default;
+  quizComponentExists = true;
+} catch (e) {
+  quizComponentExists = false;
+}
+
+let fs = require('fs');
+let quiz_data = require('../../quiz_data.json')
+let babylon = require('babylon')
+
+describe('Quiz Component', () => {
+  it('has a method named `handleClick` and a renders a QuizQuestion component with a `clickHandler` prop @quiz-has-click-handler', () => {
+    assert(quizComponentExists, "The Quiz component hasn't been created yet.")
+
+    let quiz;
+
+    try {
+      quiz = shallow(<Quiz />)
+    } catch (e) {
+      assert(false, "We weren't able to mount the Quiz component")
+    }
+
+    assert(quiz.find('QuizQuestion').length == 1, "We couldn't find the QuizQuestion component in the Quiz component's JSX.")
+    
+    assert(quiz.find('QuizQuestion').props().clickHandler != null, "The QuizQuestion tag in Quiz's JSX doesn't have a `clickHandler` property.")
+
+    assert(quiz.find('QuizQuestion').props().clickHandler.name == 'bound handleClick', "The QuizQuestion tag in Quiz's JSX has a `clickHandler` property, but the value isn't set to `this.handleClick.bind(this)`.")
+
+  })
+})
